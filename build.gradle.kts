ccceb46d70cacd6878c187936ae8c9a2d4a9ec32
jonathanfdez77 <jonathanfdez62@gmail.com>
2025-10-14 11:02:25 -0500

    🎨 refactor(resources): organización y limpieza de archivos de recursos XML
    - 🧾 Se corrigió el uso incorrecto de strings dentro de colors.xml.
    - 🎯 Se trasladaron todas las etiquetas <string> al archivo strings.xml correspondiente.
    - 🧱 Se dejó colors.xml únicamente con definiciones de colores.
    - 🧹 Se eliminaron duplicados que causaban el error "Duplicate resources".
    - 🚀 Se realizó limpieza y reconstrucción del proyecto para asegurar correcta compilación.
    - Se trabajo en reuniones en vivo con los integrantes del equipo.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.kotlinAndroid)
-    alias(libs.plugins.kotlinCompose)
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.kotlinCompose) apply false
 }
